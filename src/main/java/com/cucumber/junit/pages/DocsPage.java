07dece651c3cd31c83a9ab7843afda68568b6f55
delikate <replykate@gmail.com>
2022-03-15 20:13:37 +0200

    added methods for second test to HomePage and DocsPage

--- src/main/java/com/cucumber/junit/pages/DocsPage.java
+++ src/main/java/com/cucumber/junit/pages/DocsPage.java
@@ -1,7 +1,6 @@
 package com.cucumber.junit.pages;
 
 import com.codeborne.selenide.Condition;
-import com.cucumber.junit.webelement.DecoratedElement;
 
 import static com.codeborne.selenide.Selenide.$x;
 
@@ -9,8 +8,7 @@ public class DocsPage {
     private static final String PAGE_TITLE = "//div[@class='page-header']/*[contains(text(), '%s')]";
 
     public void isPageTitleDisplayed(String text) {
-        new DecoratedElement($x(String.format(PAGE_TITLE, text)))
-                .should(Condition.exist)
+        $x(String.format(PAGE_TITLE, text)).should(Condition.exist)
                 .should(Condition.visible);
     }
 }
--- src/main/java/com/cucumber/junit/pages/HomePage.java
+++ src/main/java/com/cucumber/junit/pages/HomePage.java
@@ -6,20 +6,22 @@ import org.openqa.selenium.By;
 
 import java.util.List;
 
+
 import static com.codeborne.selenide.Selenide.$;
+
 import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.$x;
 import static java.lang.String.format;
 
 public class HomePage {
 
     private static final String MAIN_URL = "https://cucumber.io/";
     private static final String MENU_LINK_PATTERN = "//*[contains(@class,'nav-link') and contains(text(),'%s')]";
-    private static final String CHILD_MENU_LINK_PATTERN = "//*[contains(@class,'menu-docs')]//*[contains(@class,'item item-al')]";
+    private static final String CHILD_MENU_LINKS = "//*[contains(@class,'menu-docs')]//*[contains(@class,'item item-al')]";
+    private static final String CHILD_MENU_LINK_PATTERN = "//*[contains(@class,'menu-docs')]//*[contains(@class,'item item-al') and contains(text(),'%s')]";
 
     public void openCucumberWebsite() {
         DriverManager.getDriver().get(MAIN_URL);
-        //open(MAIN_URL);
-       // getWebDriver().get(MAIN_URL);
     }
 
     public SelenideElement getMenuSection(String linkText) {
@@ -27,6 +29,10 @@ public class HomePage {
     }
 
     public List<String> getMenuItems() {
-        return $$(By.xpath(CHILD_MENU_LINK_PATTERN)).texts();
+        return $$(By.xpath(CHILD_MENU_LINKS)).texts();
+    }
+
+    public SelenideElement getMenuItem(String itemName) {
+        return $x(String.format(CHILD_MENU_LINK_PATTERN, itemName));
     }
 }
